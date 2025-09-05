c8408a4efe0b2ad3546bba7174affc365be9a494
unknown <akeem.mighty@gmail.com>
2025-09-05 17:15:49 -0400

    Start Working On Array Swap Vowels Logic

---
+++ vowel-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //Set initial loop counters
+    //Get length of array - 1
+    //Loop through first array increment index until first vowel in array is found
+    //If no vowel is found in first array return zero
+    //Loop through second array
+    //In second array check if element at current index is a vowel and if first array vowel exists swap
+    //Create did_swap function to check if swap took place and increment both integers
+    //Create element_swap function to swap current indexed elements of passed arrays
+    //Create is_end_of_array function to check if index elements exceeds limit of arrays passed update bool value to true return updated value
+    
+    return 0;
+}
